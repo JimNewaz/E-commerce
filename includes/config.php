e0eace79d14d9b821e733f64e761704d70785df8
Syed Noor E Newaz <63665452+JimNewaz@users.noreply.github.com>
2022-08-03 22:56:17 +0600

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','gamesto1_len');
-define('DB_PASS' ,'567@len567_');
+define('DB_USER','root');
+define('DB_PASS' ,'');
 define('DB_NAME', 'gamesto1_gameshop');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
